de0a2ff360099052b5b919d1c5b8eead4bda6d78
Mari-8 <ryanaltimari8@gmail.com>
2021-04-30 18:32:42 -0500

    commit tree 6d25880d42573ebf5fdad8efc66699b7866c2046

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,19 @@
-// Code EyesOnMe Component Here
+import React, { Component } from 'react'
+
+export default class EyesOnMe extends Component {
+    render() {
+        return (
+            <div>
+                <button onFocus={this.inFocus} onBlur={this.outOfFocus}></button>
+            </div>
+        )
+    }
+
+    outOfFocus = () => {
+        console.log("Hey! Eyes on me!")
+    }
+
+    inFocus = () => {
+        console.log("Good!")
+    }
+}
